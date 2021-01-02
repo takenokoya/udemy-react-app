5b331c3cc18f2b74c7e230205885ddb794aff217
takenokoya <takuya1002@gmail.com>
2021-01-02 22:08:50 +0900

    functional components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
 
-function App() {
-  // const greeting = 'Hi, Takuya!';
-  // const dom = <h1 className="foo">{greeting}</h1>;
-  // return (
-  //   dom
-  // );
-  return (
-    <React.Fragment>
-      <label htmlFor='bar'>
-        bar
-      </label>
-      <input type='test' onChange={() => { console.log('I am clicked!') }} />
-    </React.Fragment>
-    // <h1>Hello World!!</h1>
-  );
+// function App() {
+//   return (
+//     <React.Fragment>
+//       <label htmlFor='bar'>
+//         bar
+//       </label>
+//       <input type='test' onChange={() => { console.log('I am clicked!') }} />
+//     </React.Fragment>
+//     // <h1>Hello World!!</h1>
+//   );
+// }
+
+const App = () => { 
+  return <Cat />
 }
 
+const Cat = () => {
+  return (
+    <div>Meow!</div>
+  )
+}
 export default App;
