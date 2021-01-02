5f9f0668a69161e4dca9dff86ca9f8eb7b145e5e
takenokoya <takuya1002@gmail.com>
2021-01-02 22:23:38 +0900

    props

--- src/App.js
+++ src/App.js
@@ -13,12 +13,30 @@ import React from 'react';
 // }
 
 const App = () => { 
-  return <Cat />
-}
-
-const Cat = () => {
+  const profiles = [
+    { name: 'Taro', age: 10 },
+    { name: 'Hanako', age: 25 }, 
+    { name: 'Mr.Default' }, 
+  ]
   return (
-    <div>Meow!</div>
+    <div>
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index} /> 
+        })
+      }
+    </div>
   )
 }
+
+const User = (props) => {
+  return (
+  <div>Hi, I am {props.name}, and I am {props.age} years old</div>
+  )
+}
+
+User.defaultProps = {
+  age: 1
+}
+
 export default App;
