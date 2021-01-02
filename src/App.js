05fd3ad76a2cb2b27ab73a021dd1c034e758add0
takenokoya <takuya1002@gmail.com>
2021-01-02 17:18:24 +0900

    more jsx

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import React from 'react';
+
 function App() {
+  // const greeting = 'Hi, Takuya!';
+  // const dom = <h1 className="foo">{greeting}</h1>;
+  // return (
+  //   dom
+  // );
   return (
-    <h1>Hello World!!</h1>
+    <React.Fragment>
+      <label htmlFor='bar'>
+        bar
+      </label>
+      <input type='test' onChange={() => { console.log('I am clicked!') }} />
+    </React.Fragment>
+    // <h1>Hello World!!</h1>
   );
 }
 
