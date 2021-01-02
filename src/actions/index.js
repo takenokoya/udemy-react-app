2a9036299558cd784243aaa3af388458899e3eec
takenokoya <takuya1002@gmail.com>
2021-01-02 23:36:32 +0900

    action and action_creater

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
+
+export const increament = () => ({
+    type: INCREMENT
+})
+
+export const decreament = () => ({
+    type: DECREMENT
+})
