d2623619aea8ba9ef93f01504bb6aa76e1682cee
takenokoya <takuya1002@gmail.com>
2021-01-09 22:46:44 +0900

    add event_show component and add delete func

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,10 +1,13 @@
 import _ from 'lodash'
-import { readEvents, READ_EVENTS } from '../actions'
+import { readEvents, READ_EVENTS, DELETE_EVENT } from '../actions'
 
 export default (events = {}, action) => {
     switch (action.type) {
         case READ_EVENTS:
             return _.mapKeys(action.response.data, 'id') // 配列をidをキーとしたハッシュに変換
+        case DELETE_EVENT:
+            delete events[action.id]
+            return { ...events } // スプレッド演算子
         default:
             return events
     }
